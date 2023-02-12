851d46b5bdcaca99c6bf727c79f9be1661a09164
Stanislav Zyuzko <stanislav.zyuzko@yandex.ru>
2023-02-12 11:45:42 +0300

    начинаю тесты с меню

--- #src/js/burger.js
+++ #src/js/burger.js
@@ -1,15 +1,15 @@
 const { body } = document;
-const menu = document.querySelector(".header__menu-body");
-const burger = document.querySelector(".burger");
-const menuLinks = menu.querySelectorAll("a");
+const menu = document.querySelector('.header__menu-body');
+const burger = document.querySelector('.burger');
+const menuLinks = menu.querySelectorAll('a');
 
 const closeLink = () => {
-   body.classList.toggle("disable-scroll");
-   menu.classList.toggle("header__menu-body--visible");
-   burger.classList.toggle("burger--active");
+  body.classList.toggle('disable-scroll');
+  menu.classList.toggle('header__menu-body--visible');
+  burger.classList.toggle('burger--active');
 };
 
-burger.addEventListener("click", closeLink);
+burger.addEventListener('click', closeLink);
 menuLinks.forEach((elem) => {
-   elem.addEventListener("click", closeLink);
+  elem.addEventListener('click', closeLink);
 });
