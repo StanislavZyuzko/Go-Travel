bdbe94b4c5fe5afebf87765c8af251fe8ed686c9
Stanislav Zyuzko <stanislav.zyuzko@yandex.ru>
2023-02-12 12:56:43 +0300

    добавил в код функцию

--- #src/js/burger.js
+++ #src/js/burger.js
@@ -1,19 +1,15 @@
 const { body } = document;
-console.log('testConnect');
-const menu = document.querySelector('.header__menu-body');
-const menuLinks = menu.querySelectorAll('a');
-const burger = document.querySelector('.burger');
+const menu = document.querySelector(".header__menu-body");
+const menuLinks = menu.querySelectorAll("a");
+const burger = document.querySelector(".burger");
 
-burger.addEventListener('click', () => {
-  body.classList.toggle('disable-scroll');
-  menu.classList.toggle('header__menu-body--visible');
-  burger.classList.toggle('burger--active');
-});
+const openLink = () => {
+   body.classList.toggle("disable-scroll");
+   menu.classList.toggle("header__menu-body--visible");
+   burger.classList.toggle("burger--active");
+};
 
-menuLinks.forEach(element => {
-    element.addEventListener('click', () => {
-        body.classList.toggle('disable-scroll');
-        menu.classList.toggle('header__menu-body--visible');
-        burger.classList.toggle('burger--active');
-      });
+burger.addEventListener("click", openLink);
+menuLinks.forEach((element) => {
+   element.addEventListener("click", openLink);
 });
