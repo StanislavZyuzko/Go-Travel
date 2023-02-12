3270240023196f9cc9077b9a0afc0988bda9008b
Stanislav Zyuzko <stanislav.zyuzko@yandex.ru>
2023-02-12 12:47:57 +0300

    еще тест с сылками для телефона

--- #src/js/burger.js
+++ #src/js/burger.js
@@ -1,10 +1,19 @@
 const { body } = document;
 console.log('testConnect');
 const menu = document.querySelector('.header__menu-body');
+const menuLinks = menu.querySelectorAll('a');
 const burger = document.querySelector('.burger');
 
 burger.addEventListener('click', () => {
   body.classList.toggle('disable-scroll');
   menu.classList.toggle('header__menu-body--visible');
   burger.classList.toggle('burger--active');
+});
+
+menuLinks.forEach(element => {
+    element.addEventListener('click', () => {
+        body.classList.toggle('disable-scroll');
+        menu.classList.toggle('header__menu-body--visible');
+        burger.classList.toggle('burger--active');
+      });
 });
--- GoTravel/js/main.js
+++ GoTravel/js/main.js
@@ -54,12 +54,21 @@ ltrs.forEach((ltr) => {
 const { body } = document;
 console.log('testConnect');
 const menu = document.querySelector('.header__menu-body');
+const menuLinks = menu.querySelectorAll('a');
 const burger = document.querySelector('.burger');
 
 burger.addEventListener('click', () => {
   body.classList.toggle('disable-scroll');
   menu.classList.toggle('header__menu-body--visible');
   burger.classList.toggle('burger--active');
+});
+
+menuLinks.forEach(element => {
+    element.addEventListener('click', () => {
+        body.classList.toggle('disable-scroll');
+        menu.classList.toggle('header__menu-body--visible');
+        burger.classList.toggle('burger--active');
+      });
 });
 ;
 const CLASS_LIST = {
