71f8ebe2db5b6f88188075f6ec86658b48ddafef
Stanislav Zyuzko <stanislav.zyuzko@yandex.ru>
2023-02-12 12:18:26 +0300

    тест 3

--- #src/js/burger.js
+++ #src/js/burger.js
@@ -1,16 +1,9 @@
 const { body } = document;
 const menu = document.querySelector('.header__menu-body');
 const burger = document.querySelector('.burger');
-// const menuLinks = menu.querySelectorAll('a');
 
-const closeLink = () => {
+burger.addEventListener('click', () => {
   body.classList.toggle('disable-scroll');
   menu.classList.toggle('header__menu-body--visible');
   burger.classList.toggle('burger--active');
-};
-
-burger.addEventListener('click', closeLink);
-
-// menuLinks.forEach((elem) => {
-//   elem.addEventListener('click', closeLink);
-// });
+});
