36d8eb51bff121b8ba1c912312ee17a70348b307
Stanislav Zyuzko <stanislav.zyuzko@yandex.ru>
2023-02-12 11:38:33 +0300

    сделал закрытие ссылки по клику

--- #src/js/burger.js
+++ #src/js/burger.js
@@ -1,8 +1,15 @@
 const { body } = document;
-const menu = document.querySelector('.header__menu-body');
-const burger = document.querySelector('.burger');
-burger.addEventListener('click', () => {
-  body.classList.toggle('disable-scroll');
-  menu.classList.toggle('header__menu-body--visible');
-  burger.classList.toggle('burger--active');
+const menu = document.querySelector(".header__menu-body");
+const burger = document.querySelector(".burger");
+const menuLinks = menu.querySelectorAll("a");
+
+const closeLink = () => {
+   body.classList.toggle("disable-scroll");
+   menu.classList.toggle("header__menu-body--visible");
+   burger.classList.toggle("burger--active");
+};
+
+burger.addEventListener("click", closeLink);
+menuLinks.forEach((elem) => {
+   elem.addEventListener("click", closeLink);
 });
