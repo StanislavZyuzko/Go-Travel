33398b61206972997c549857cf841d132b9ce1ec
Stanislav Zyuzko <stanislav.zyuzko@yandex.ru>
2023-02-12 09:16:00 +0300

    закоммител избыточный html и scss

--- GoTravel/js/main.js
+++ GoTravel/js/main.js
@@ -55,7 +55,7 @@ const { body } = document;
 const menu = document.querySelector('.header__menu-body');
 const burger = document.querySelector('.burger');
 burger.addEventListener('click', () => {
-  body.classList.toggle('stop-scroll');
+  body.classList.toggle('disable-scroll');
   menu.classList.toggle('header__menu-body--visible');
   burger.classList.toggle('burger--active');
 });
