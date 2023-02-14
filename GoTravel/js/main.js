c4bfe2530a4396e7f8ec84c20cef8ab118e5e645
Stanislav Zyuzko <stanislav.zyuzko@yandex.ru>
2023-02-14 11:51:15 +0300

    переписал тест бургера через флаг

--- GoTravel/js/main.js
+++ GoTravel/js/main.js
@@ -55,23 +55,33 @@ const { body } = document;
 const menu = document.querySelector('.header__menu-body');
 const menuLinks = menu.querySelectorAll('a');
 const burger = document.querySelector('.burger');
+let burgerActive = false;
 
 const openLink = () => {
   body.classList.toggle('disable-scroll');
   menu.classList.toggle('header__menu-body--visible');
   burger.classList.toggle('burger--active');
+  burgerActive = !burgerActive;
 };
 
-burger.addEventListener('click', openLink);
+burger.addEventListener('click', () =>{
+  openLink();
+
+
+});
 
 menuLinks.forEach((element) => {
   element.addEventListener('click', () => {
-    const burgerActive = document.querySelector('.burger--active');
+    // const burgerActive = document.querySelector('.burger--active');
     if (burgerActive) {
       openLink();
     }
   });
 });
+
+document.onclick = () => {
+console.log(burgerActive);
+}
 ;
 const CLASS_LIST = {
    MODAL: "modal",
